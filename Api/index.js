0a19466f1c681f80cb9cc135d66d9eeba5f8d24a
Lav Yadav <yadavlav571@gmail.com>
2024-03-28 13:28:07 +0530

    Connect to Database

--- Api/index.js
+++ Api/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-const app=express();
 
-app.listen(3000,()=>{
-    console.log(`Server is running on port 3000 !!`);
-})
+const url='mongodb+srv://yadavlav571:Corona@mern-blog-webapp.ccan9i6.mongodb.net/Mern-Blog-WebApp?retryWrites=true&w=majority&appName=Mern-Blog-WebApp';
+
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('Connection successful');
+  })
+  .catch((err) => {
+    console.log("Fail");
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000 !!');
+});
