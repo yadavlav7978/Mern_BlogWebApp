55e7b1caf484b66d8a2d0269f3820edb6c8955aa
Lav Yadav <yadavlav571@gmail.com>
2024-03-28 11:33:06 +0530

    Database Connection

---
+++ Api/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app=express();
+
+app.listen(3000,()=>{
+    console.log(`Server is running on port 3000 !!`);
+})
---
+++ Client/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function signUp() {
+  return (
+    <h1>
+      SignUp
+    </h1>
+  )
+}
