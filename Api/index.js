3aae3cce980c59867e79aa3ec205683c39e6fef8
Lav Yadav <yadavlav571@gmail.com>
2024-03-28 20:19:39 +0530

    Create User Model

--- Api/index.js
+++ Api/index.js
@@ -17,5 +17,5 @@ mongoose.connect(url)
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000 !!');
+  console.log('Server is running on port 3000 ');
 });
