1828664a855c6208fb935bb53841967714882b23
Lav Yadav <yadavlav571@gmail.com>
2024-04-20 14:14:30 +0530

    Add admin functionality to the user

--- Api/controller/auth.controller.js
+++ Api/controller/auth.controller.js
@@ -3,10 +3,16 @@ import { errorHandler } from "../utilis/error.js";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-
   const { username, email, password } = req.body;
 
-  if (!username || !email || !password || username === "" || email === "" || password === "") {
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
     return next(errorHandler(400, "All fields are required"));
   }
 
@@ -51,56 +57,74 @@ export const signin = async (req, res, next) => {
     }
 
     // Generating JWT token with user ID as payload and signing it with the secret key
-    const token = jwt.sign({ id: validUser._id }, secretKey);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      secretKey
+    );
 
     // Sending the token in a cookie named 'access_token'
-    res.cookie("access_token", token, { httpOnly: true }).status(200).json(validUser);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(validUser);
   } catch (error) {
     next(error);
   }
 };
 
 export const google = async (req, res, next) => {
+  const { email, name, googlePhotoUrl } = req.body;
+  try {
+    // Check if user already exists
+    const existingUser = await user.findOne({ email });
 
-    const { email, name, googlePhotoUrl } = req.body;
-    try {
-      // Check if user already exists
-      const existingUser = await user.findOne({ email });
-      
-      if (existingUser) {
-        // User exists, generate JWT token
-        const secretKey = "lav571";
-        const token = jwt.sign({ id: existingUser._id }, secretKey);
+    if (existingUser) {
+      // User exists, generate JWT token
+      const secretKey = "lav571";
+      const token = jwt.sign(
+        { id: existingUser._id, isAdmin: existingUser.isAdmin },
+        secretKey
+      );
 
-        res.cookie("access_token", token, { httpOnly: true }).status(200).json(existingUser);
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(existingUser);
+    } else {
+      // User doesn't exist, create a new user
+      const generatedPassword = Math.random().toString(36).slice(-8); // Generate Random Password
 
-      } else {
-        // User doesn't exist, create a new user
-        const generatedPassword = Math.random().toString(36).slice(-8); // Generate Random Password
+      const newUser = new user({
+        username:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: generatedPassword, // Assign generated password
+        profilePicture: googlePhotoUrl,
+      });
 
-        const newUser = new user({
-          username: name.toLowerCase().split(" ").join("") + Math.random().toString(9).slice(-4),
-          email,
-          password: generatedPassword, // Assign generated password
-          profilePicture: googlePhotoUrl,
-        });
-
-        await newUser.save();
-        // Generate JWT token
-        const secretKey = "lav571";
-        const token = jwt.sign({ id: newUser._id }, secretKey);
-        res.cookie("access_token", token, { httpOnly: true }).status(200).json(newUser);
-      }
-    } catch (error) {
-      // Handle errors
-    
-      res.status(500).json({ message: "Internal server error" });
+      await newUser.save();
+      // Generate JWT token
+      const secretKey = "lav571";
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        secretKey
+      );
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(newUser);
     }
-  };
-  
+  } catch (error) {
+    // Handle errors
 
-  //!----------------------------------------- JSON web Token----------------------------------------------------------------
-  {/*
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+//!----------------------------------------- JSON web Token----------------------------------------------------------------
+{
+  /*
     JSON web Token is used to authenticate the user and also help in signIn without requiring users to repeatedly enter their username and password.
     How its works?
     1) During login, the user enters their credentials (username/password). The server validates them. 
@@ -109,5 +133,5 @@ export const google = async (req, res, next) => {
     2) Then server sends the JWT token back to the client. This token can be stored securely in a cookie or local storage named "access_token".
     3) With each subsequent request to the server that needs authentication, the client used this JWT token to authorize and  does not repeatedly enter their username and password.
 
-*/}
-
+*/
+}
--- Api/models/user.model.js
+++ Api/models/user.model.js
@@ -1,29 +1,34 @@
 import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema({
-
-       username:{
-        type:String,
-        required:true,
-        unique:true,
-       },
-       email:{
-        type:String,
-        required:true,
-        unique:true,
-       },
-       password:{
-             type:String,
-             required:true,
-       },
-       profilePicture:{
-            type:String,
-            default:"https://storage.needpix.com/rsynced_images/blank-profile-picture-973460_1280.png",
-       }
-       
-      },{timestamps:true}
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://storage.needpix.com/rsynced_images/blank-profile-picture-973460_1280.png",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
 
-const user=mongoose.model('user',userSchema);
+const user = mongoose.model("user", userSchema);
 
 export default user;
