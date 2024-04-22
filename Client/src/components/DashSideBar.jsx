66ea31910863c7652d46e5372e870d361fd843b1
Lav Yadav <yadavlav571@gmail.com>
2024-04-22 17:34:21 +0530

    Add posts section to the dashboard

--- Client/src/components/DashSideBar.jsx
+++ Client/src/components/DashSideBar.jsx
@@ -1,15 +1,17 @@
 import { Sidebar } from "flowbite-react";
-import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { HiUser, HiArrowSmRight, HiDocumentText } from "react-icons/hi";
 import React, { useEffect, useState } from "react";
 import { useLocation, Link, useNavigate } from "react-router-dom";
 import { signOutSuccess } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function DashSideBar() {
   const location = useLocation(); //It is used to access the current location of object in our component.
   const [tab, setTab] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     // In this case, the UseEffect should only run when the location.search value changes.
@@ -42,18 +44,29 @@ export default function DashSideBar() {
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
-              label={"User"}
+              label={currentUser.isAdmin ? "Admin" : "User"}
               labelColor="dark"
               as="div"
             >
               Profile
             </Sidebar.Item>
           </Link>
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab === "posts"}
+                icon={HiDocumentText}
+                as="div"
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
 
           <Sidebar.Item
             icon={HiArrowSmRight}
--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashProfile from "../components/DashProfile.jsx";
 import DashSideBar from "../components/DashSideBar.jsx";
+import DashPosts from "../components/DashPosts.jsx";
 
 export default function Dashboard() {
   const location = useLocation(); //It is used to access the current location of object in our component.
@@ -27,6 +28,8 @@ export default function Dashboard() {
       {/*------------------------------Profile------------------------- */}
       {tab === "profile" && <DashProfile />}{" "}
       {/*It will only 'DashProfile' when tab===profile*/}
+      {/*----------------------Posts-------------------------*/}
+      {tab === "posts" && <DashPosts />}
     </div>
   );
 }
